269895714b2354a04037b3716887fe514c590852
brunofoster03 <bruno.7de12@gmail.com>
2023-06-30 13:17:11 -0300

    New Enterprise controller
    Created postEnterprise.js controller
    Updated router index.js with new route (/enterprise/create)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const postAuth = require('../controllers/Auth/postAuth')
 const putUserData = require('../controllers/User/putUserData')
+const postEnterprise = require('../controllers/Enterprise/postEnterprise')
 
 // Create router
 const router = Router()
@@ -14,6 +15,8 @@ router.get('/test', (_req, res) => {
 router.post('/auth', postAuth)
 //Update user data
 router.put('/user/update', putUserData)
+//Create enterprise
+router.post('/enterprise/create', postEnterprise)
 
 // Exports router
 module.exports = router
