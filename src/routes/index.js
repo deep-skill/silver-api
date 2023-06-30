f0f7555f5d87b8b875f6f65ef00f7774b5baafb0
brunofoster03 <bruno.7de12@gmail.com>
2023-06-30 12:34:44 -0300

    New User controller
    Created putUserData.js controller
    Updated router index.js with new route (/user/update)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const postAuth = require('../controllers/Auth/postAuth')
+const putUserData = require('../controllers/User/putUserData')
 
 // Create router
 const router = Router()
@@ -9,7 +10,10 @@ router.get('/test', (_req, res) => {
   res.send('connected')
 })
 
+//Auth new user
 router.post('/auth', postAuth)
+//Update user data
+router.put('/user/update', putUserData)
 
 // Exports router
 module.exports = router
